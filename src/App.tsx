28ea75ce33daba59a6a710590404f73ee58fa1ba
swalker326 <shane@swalker.dev>
2024-03-15 06:18:09 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h2>Hello</h2>
+        <p>
+          For this interview, you'll be utilizing the{' '}
+          <a href="https://docs.potterdb.com/apis/rest">Potter DB</a> to fetch a
+          list of characters from Harry Potter. We would like you to retrieve
+          all characters who's name matches 'albus'
+        </p>
+      </div>
+      List matches here
+    </div>
+  );
+}
+
+export default App;
